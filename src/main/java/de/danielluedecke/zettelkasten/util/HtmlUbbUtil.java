1ac4191b99f5a5392e346055f8b9f118a2765245
Ralf Barkow <>
2020-05-02 17:56:34 +0200

    HtmlUbbUtil.java: Typo in comment

--- src/main/java/de/danielluedecke/zettelkasten/util/HtmlUbbUtil.java
+++ src/main/java/de/danielluedecke/zettelkasten/util/HtmlUbbUtil.java
@@ -1325,7 +1325,7 @@ public class HtmlUbbUtil {
                                 StringBuilder resize = new StringBuilder("");
                                 // here we extract the original img-source and add a
                                 // width and height attribute. furthermore, the image is
-                                // surrounded by a hyperlink-tag that referrs to the original image
+                                // surrounded by a hyperlink-tag that refers to the original image
                                 if (!isExport) {
                                     resize.append("<a href=\"");
                                     resize.append(dummy.substring(startOfSrcValue, end));
